ffe806942d662ddd22c3a00bc6b689f062067a98
GiovannyCortes <giovannyandres.cortes@outlook.com>
2023-05-15 14:41:39 +0200

    v1.2.4 (keyvault implementado con google maps)

---
+++ Hairdressers_Azure/Controllers/SecretController.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using Hairdressers_Azure.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hairdressers_Azure.Controllers {
+    public class SecretController : Controller {
+
+        private SecretClient secretClient;
+
+        public SecretController(SecretClient secretClient) {
+            this.secretClient = secretClient;
+        }
+
+        [AuthorizeUsers] [HttpGet]
+        public async Task<IActionResult> GetSecret(string secretName) {
+            KeyVaultSecret secret = await secretClient.GetSecretAsync(secretName);
+            return Json(secret.Value);
+        }
+
+    }
+}
---
+++ Hairdressers_Azure/wwwroot/js/google_maps.js
@@ -0,0 +1,47 @@
+﻿function getGoogleMapsIframe(address) {
+    $.ajax({
+        url: '/Secret/GetSecret?secretName=googlemapsapikey',
+        method: 'GET',
+        success: function (data) {
+            var mapUrl = `https://www.google.com/maps/embed/v1/place?q=${encodeURIComponent(address)}&key=` + data;
+            // Creamos el objeto iframe que será devuelto por la función
+            const iframe = $('<iframe></iframe>', {
+                'src': mapUrl,
+                'width': '600',
+                'height': '450',
+                'frameborder': '0',
+                'style': 'border:0'
+            });
+            // Agregamos el iframe a nuestro enlace
+            $('a.button_suggestions:last').append(iframe);
+        },
+        error: function (xhr, status, error) {
+            console.log('Error retrieving secret: ' + error);
+        }
+    });
+}
+
+//function getGoogleMapsIframe(address) {
+//    var mapUrl = "";
+//    debugger;
+//    $.ajax({
+//        url: '/Secret/GetSecret?secretName=googlemapsapikey',
+//        method: 'GET',
+//        success: function (data) {
+//            debugger;
+//            mapUrl = `https://www.google.com/maps/embed/v1/place?q=${encodeURIComponent(address)}&key=` + data;
+//        },
+//        error: function (xhr, status, error) {
+//            console.log('Error retrieving secret: ' + error);
+//        }
+//    });
+//    // Creamos el objeto iframe que será devuelto por la función
+//    const iframe = $('<iframe></iframe>', {
+//        'src': mapUrl,
+//        'width': '600',
+//        'height': '450',
+//        'frameborder': '0',
+//        'style': 'border:0'
+//    });
+//    return iframe;
+//}
